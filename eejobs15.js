4e3ba84a9590b732b0d300cbbd5a084268657038
sh8kme <starrje@hotmail.com>
2020-08-31 12:09:59 -0700

    Update and rename eejobs14.js to eejobs15.js

--- eejobs15.js
+++ eejobs15.js
@@ -10,8 +10,11 @@ function getJobs() {
 
 function successFunc(t) {
 
-    return $(".loading").hide(), console.log(url), $("#Careers").addClass(t.currentPage), 25 <= t.count ? $("#loadMore").show() : $("#loadMore").hide(), $.each(t, function(t, e) {
-
+    return $(".loading").hide(), console.log(url), v = t.currentPage , 25 <= t.count ? $("#loadMore").show() : $("#loadMore").hide(), $.each(t, function(t, e) {
+$("#Careers").json2html(l, {
+                
+                class: "${currentPage}"
+            }),
         $.each(e, function(t, e) {
             let l = [e];
             $("#Careers").json2html(l, {
