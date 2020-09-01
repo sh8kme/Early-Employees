fa508ed768d9306eb57108ad8fe9128604125f1f
sh8kme <starrje@hotmail.com>
2020-08-31 19:44:13 -0700

    Update eejobs18.js

--- eejobs18.js
+++ eejobs18.js
@@ -10,11 +10,7 @@ function getJobs() {
 
 function successFunc(t) {
 
-    return $(".loading").hide(), console.log(url), v = t.currentPage , 25 <= t.count ? $("#loadMore").show() : $("#loadMore").hide(), $("#Careers").json2html( t, {
-                
-                class: "${currentPage}"
-            }),
-    $.each(t, function(t, e) {
+    return $(".loading").hide(), console.log(url), $('body').addClass(t.currentPage) , 25 <= t.count ? $("#loadMore").show() : $("#loadMore").hide(), $.each(t, function(t, e) {
 
         $.each(e, function(t, e) {
             let l = [e];
