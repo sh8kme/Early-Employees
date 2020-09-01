d2e4501bf78db94bfff62c6261ef8bbb9597df62
sh8kme <starrje@hotmail.com>
2020-08-31 19:59:45 -0700

    Update and rename eejobs24.js to eejobs25.js

--- eejobs25.js
+++ eejobs25.js
@@ -9,7 +9,7 @@ function getJobs() {
 }
 
 function successFunc(t) {
-v = t.currentPage;
+z = t.currentPage;
     return $(".loading").hide(), console.log(url), $('#Filters').addClass(t.currentPage) , 25 <= t.count ? $("#loadMore").show() : $("#loadMore").hide(), $.each(t, function(t, e) {
 
         $.each(e, function(t, e) {
@@ -318,7 +318,7 @@ j = "", "undefined" !== getUrlParameter("job") && (j = getUrlParameter("job"), i
     var t = $(this).text();
     $("a.ee-active:contains(" + t + ")")[0].click()
 }), $("#loadMore").on("click", function(t) {
-    limit = $(this).attr("data-limit"), console.log(v), start = (t.currentPage) + 1 , loc = "", blkstr = [], $(".location .ee-active").each(function(t, e) {
+    limit = $(this).attr("data-limit"), console.log(z+1), start = (t.currentPage) + 1 , loc = "", blkstr = [], $(".location .ee-active").each(function(t, e) {
         var l = $(this).html();
         blkstr.push(l), console.log(blkstr.join("&location=")), loc = blkstr.join("&location=")
     }), acc = "", accstr = [], $(".accelerator .ee-active").each(function(t, e) {
