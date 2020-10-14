12a25e3c9e1273e57391566483d737d68fc63c7a
sh8kme <starrje@hotmail.com>
2020-10-14 08:42:02 -0700

    Update and rename eejobs45.js to eejobs46.js

--- eejobs46.js
+++ eejobs46.js
@@ -304,9 +304,9 @@ j = "", "undefined" !== getUrlParameter("job") && (j = getUrlParameter("job"), i
         var l = $(this).html();
         invstr.push(l), console.log(accstr.join("&investor=")), inv = invstr.join("&investor=")
     }), rol = "", rolr = [], $(".role .ee-active").each(function(t, e) {
-        var l = $(this).html();
-        l.replace('end','');
-        l.replace('-','');
+        var q = $(this).html();
+        var r = q.replace('end','');
+        var l = r.replace('-','');
         rolr.push(l), console.log(rolr.join("&jobtitle=")), rol = rolr.join("&jobtitle=")
     }), siz = "", size = [], $(".size .ee-active").each(function(t, e) {
         var p = $(this).html();
