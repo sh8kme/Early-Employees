bb36bf8536f9ffa6a62b828e4aa80d46b7c8de4d
sh8kme <starrje@hotmail.com>
2020-10-17 11:20:37 -0700

    Update and rename locator8.js to locator9.js

--- locator9.js
+++ locator9.js
@@ -219,7 +219,8 @@ $("#fireSearch .w-dropdown-link").on("click", function() {
                     if (roundedDistance < 100) {
                         details.innerHTML +=
                             '<p class="l-distance"><strong>' + roundedDistance + ' kms away</strong></p>';
-                     '<a href="profile?profile=' + currentFeature.properties.mid + '" target="_blank" class="l-profile-link">View Profile &#10230;</a>'
+                     '<a href="profile?profile=' + prop.mid + '" target="_blank" class="l-profile-link">View Profile &#10230;</a>';
+                        
                     }
                 }
                 link.addEventListener('click', function(e) {
