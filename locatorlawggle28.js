ab99967bea2a1e3acf494e1bc55a2b40bf4b4013
sh8kme <starrje@hotmail.com>
2021-04-29 13:46:42 -0700

    Update and rename locatorlawggle27.js to locatorlawggle28.js

--- locatorlawggle28.js
+++ locatorlawggle28.js
@@ -336,7 +336,7 @@ $('.next.button').on('click', function () {
 });
 $("#geocoder").keydown(function() {
  //$('.next.button').removeClass('gone');
-$(".suggestions-wrapper").click(function(){
+$(".suggestions-wrapper ul li").click(function(){
 
 $('.next.button').trigger('tap');
 });
