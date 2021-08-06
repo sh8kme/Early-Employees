98e0e319d752445c23ee894110f1814ffa21220a
sh8kme <starrje@hotmail.com>
2021-08-06 11:27:52 -0700

    Update and rename locatorlawggle38.js to locatorlawggle39.js

--- locatorlawggle39.js
+++ locatorlawggle39.js
@@ -279,7 +279,7 @@ map.addControl(new mapboxgl.NavigationControl());
            if(!$('.item.active').length){
                $('.no-results').addClass('display');
            }
-	         if(!$('.item.active.active-d').length){
+	         if(!$('.active-d').length){
                $('.no-results').addClass('display');
 			 $('.map').css("visibility","hidden");
            }
