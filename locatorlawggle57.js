49df900931d85a7e013d51ec05a824d9822cd14f
sh8kme <starrje@hotmail.com>
2021-08-09 11:36:12 -0700

    Update and rename locatorlawggle56.js to locatorlawggle57.js

--- locatorlawggle57.js
+++ locatorlawggle57.js
@@ -285,7 +285,8 @@ map.addControl(new mapboxgl.NavigationControl());
     (
         (elem) ? 
 	    $('.map').css("visibility","visible") :
-	    $('.map').css("visibility","hidden")
+	    $('.map').css("visibility","hidden");
+	    $('.no-results').addClass('display');
        // console.log(elem+' was found.') :
        // console.log('not found')
     )
